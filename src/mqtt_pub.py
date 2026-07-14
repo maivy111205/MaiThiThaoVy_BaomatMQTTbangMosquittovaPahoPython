9bd888747f3af138fabb59f7f0be3ce36b5ffafb
maivy111205 <maithithaovy632@gmail.com>
2026-07-14 19:26:10 +0700

    Update mqtt_pub.py

--- src/mqtt_pub.py
+++ src/mqtt_pub.py
@@ -1,11 +1,1 @@
-import paho.mqtt.client as mqtt
-import json
 
-client = mqtt.Client()
-client.username_pw_set("sensor", "matkhau_sensor") 
-client.connect("localhost", 1883, 60)
-
-data = {"temperature": 25.5}
-client.publish("iot/sensor/temp", json.dumps(data))
-print("Sent: Đã gửi dữ liệu thành công")
-client.disconnect()
