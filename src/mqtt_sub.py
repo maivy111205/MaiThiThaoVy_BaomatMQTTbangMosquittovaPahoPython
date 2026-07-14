f99660a5809bd2068c09a63e65e2c11ffdb83896
maivy111205 <maithithaovy632@gmail.com>
2026-07-14 19:26:23 +0700

    Update mqtt_sub.py

--- src/mqtt_sub.py
+++ src/mqtt_sub.py
@@ -1,11 +1,1 @@
-import paho.mqtt.client as mqtt
 
-def on_message(client, userdata, message):
-    print(f"Received: {message.payload.decode()}")
-
-client = mqtt.Client()
-client.username_pw_set("dashboard", "matkhau_dashboard")
-client.on_message = on_message
-client.connect("localhost", 1883, 60)
-client.subscribe("iot/sensor/temp")
-client.loop_forever()
