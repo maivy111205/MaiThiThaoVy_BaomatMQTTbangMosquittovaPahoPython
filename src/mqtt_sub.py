2e258e156679602a2ccbd5c995ecc08b7afa9cfe
maivy111205 <maithithaovy632@gmail.com>
2026-07-14 19:33:10 +0700

    Update mqtt_sub.py
    Add mqtt subscriber

--- src/mqtt_sub.py
+++ src/mqtt_sub.py
@@ -1,1 +1,40 @@
+import paho.mqtt.client as mqtt
 
+BROKER = "localhost"
+PORT = 1883
+TOPIC = "iot/sensor/temp"
+
+USERNAME = "dashboard"
+PASSWORD = "123456"
+
+LOG_FILE = "../results/logs/mqtt_log.txt"
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to Broker")
+        client.subscribe(TOPIC)
+    else:
+        print("Connection Failed")
+
+def on_message(client, userdata, msg):
+    message = msg.payload.decode()
+
+    print("\n==============================")
+    print("Message Received")
+    print("Topic:", msg.topic)
+    print("Payload:", message)
+    print("==============================")
+
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        f.write(f"{msg.topic} : {message}\n")
+
+client = mqtt.Client()
+
+client.username_pw_set(USERNAME, PASSWORD)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(BROKER, PORT, 60)
+
+client.loop_forever()
