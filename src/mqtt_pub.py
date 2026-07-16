e7611038d630e6b1bea04773d1911be45bb1d7fb
maivy111205 <maithithaovy632@gmail.com>
2026-07-16 17:40:32 +0700

    Update mqtt_pub.py

--- src/mqtt_pub.py
+++ src/mqtt_pub.py
@@ -7,7 +7,7 @@ PORT = 1883
 TOPIC = "iot/sensor/temp"
 
 USERNAME = "sensor"
-PASSWORD = "<mật_khẩu_thử_nghiệm>"
+PASSWORD = "1234"
 
 client = mqtt.Client()
 client.username_pw_set(USERNAME, PASSWORD)
